bf6f2e42c2753adbe3e7210e6010c0255e91aa5c
OpenTheDoor <tkddnwkdb@gmail.com>
2023-06-11 01:21:31 +0900

    feat: Add Bible Entity

--- src/main/java/com/example/thebible/core/entity/Bible.java
+++ src/main/java/com/example/thebible/core/entity/Bible.java
@@ -1,2 +1,30 @@
-package com.example.thebible.core.entity;public class Bible {
+package com.example.thebible.core.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "bible_korHRV")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bible {
+
+    @Id
+    @Column(name = "book")
+    private int book;
+
+    @Column(name = "chapter")
+    private int chapter;
+
+    @Column(name = "verse")
+    private int verse;
+
+    @Column(name = "content")
+    private String content;
 }

b6d3c97ee25d0e7d3ad8996869b6bdecac5e183c
OpenTheDoor <tkddnwkdb@gmail.com>
2023-06-17 21:04:38 +0900

    refactor: @AllArgsConstructor 애노테이션 추가

--- src/main/java/com/example/thebible/dto/BibleChapterRequestDto.java
+++ src/main/java/com/example/thebible/dto/BibleChapterRequestDto.java
@@ -1,10 +1,13 @@
 package com.example.thebible.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class BibleChapterRequestDto {
 
     private int book;
--- src/main/java/com/example/thebible/dto/BibleRequestDto.java
+++ src/main/java/com/example/thebible/dto/BibleRequestDto.java
@@ -1,10 +1,12 @@
 package com.example.thebible.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class BibleRequestDto {
 
     private int book;

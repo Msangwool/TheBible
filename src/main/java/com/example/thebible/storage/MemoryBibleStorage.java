bdde093f893a678f4ae2553ac481c1d7922bad21
OpenTheDoor <tkddnwkdb@gmail.com>
2023-06-25 00:51:17 +0900

    feat: BibleStorage 구현체 MemoryBibleStorage 추가

---
+++ src/main/java/com/example/thebible/storage/MemoryBibleStorage.java
@@ -0,0 +1,21 @@
+package com.example.thebible.storage;
+
+import com.example.thebible.dto.BibleResponseDto;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MemoryBibleStorage implements BibleStorage{
+
+    protected List<BibleResponseDto> todayBibleWords = new ArrayList<>();
+
+    public List<BibleResponseDto> getTodayBibleWords() {
+        return todayBibleWords;
+    }
+
+    public void updateTodayBibleWords(List<BibleResponseDto> todayBibleWords) {
+        this.todayBibleWords = todayBibleWords;
+    }
+}

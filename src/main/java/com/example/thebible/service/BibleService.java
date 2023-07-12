af6ae7a5068f6cb2a5487121c5ee92b664759351
OpenTheDoor <tkddnwkdb@gmail.com>
2023-07-13 00:43:46 +0900

    feat: Method 추가, 절과 장을 등록할 수 있도록 기존에 존재하는 Service 사용

--- src/main/java/com/example/thebible/service/BibleService.java
+++ src/main/java/com/example/thebible/service/BibleService.java
@@ -1,8 +1,6 @@
 package com.example.thebible.service;
 
-import com.example.thebible.dto.BibleChapterRequestDto;
-import com.example.thebible.dto.BibleRequestDto;
-import com.example.thebible.dto.BibleResponseDto;
+import com.example.thebible.dto.*;
 
 import java.util.List;
 
@@ -24,9 +22,15 @@ public interface BibleService {
 
     /**
      * 특정 범위에 대한 성경 말씀을 검색합니다. 검색 후, 반환하지 않고 해당 범위의 리스트를 메모리 적재 요청합니다.
-     * @param bibleRequestDtoList 검색할 성경 말씀 리스트를 받아옵니다.
+     * @param bibleRangeReqDto 범위를 측정할 수 있는 값들을 요소로 받아옵니다.
      */
-    void setBiblesRequestRange(List<BibleRequestDto> bibleRequestDtoList);
+    void setBibleRange(BibleRangeReqDto bibleRangeReqDto);
+
+    /**
+     * 특정 범위에 대한 성경 말씀을 검색하고, 해당 리스트를 메모리에 올려놓습니다.
+     * @param bibleRangeChapterReqDto 절 구간을 가져옵니다.
+     */
+    void setBibleRangeByChapter(BibleRangeChapterReqDto bibleRangeChapterReqDto);
 
     /**
      * 오늘의 성경 말씀을 조회합니다.
--- src/main/java/com/example/thebible/service/BibleServiceImpl.java
+++ src/main/java/com/example/thebible/service/BibleServiceImpl.java
@@ -1,17 +1,18 @@
 package com.example.thebible.service;
 
-import com.example.thebible.dto.BibleChapterRequestDto;
-import com.example.thebible.dto.BibleRequestDto;
-import com.example.thebible.dto.BibleResponseDto;
+import com.example.thebible.dto.*;
 import com.example.thebible.repository.BibleRepository;
 import com.example.thebible.storage.BibleStorage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BibleServiceImpl implements BibleService {
 
     private final BibleStorage bibleStorage;
@@ -25,8 +26,67 @@ public class BibleServiceImpl implements BibleService {
         return bibleRepository.findBiblesByChapter(bibleChapterRequestDto).stream().map(BibleResponseDto::buildDto).toList();
     }
 
-    public void setBiblesRequestRange(List<BibleRequestDto> bibleRequestDtoList) {
-        List<BibleResponseDto> bibleResponseDtoList = bibleRepository.findBiblesByRange(bibleRequestDtoList).stream().map(BibleResponseDto::buildDto).toList();
+    public void setBibleRange(BibleRangeReqDto bibleRangeReqDto) {
+        int startPoint = bibleRangeReqDto.getStartVerse();
+        int endPoint = bibleRangeReqDto.getEndVerse();
+        try {
+            if (startPoint > endPoint) {
+                throw new NullPointerException();
+            }
+        } catch (NullPointerException e) {
+            log.info("NullPointerException = {}", e.getMessage());
+        }
+
+        BibleRequestDto bibleRequestDto = BibleRequestDto.builder()
+                .book(bibleRangeReqDto.getBook())
+                .chapter(bibleRangeReqDto.getChapter())
+                .build();
+
+        List<BibleResponseDto> bibleResponseDtoList = new ArrayList<>();
+        while (startPoint <= endPoint) {
+
+            bibleRequestDto.setVerse(startPoint);
+            bibleResponseDtoList.add(BibleResponseDto.buildDto(bibleRepository.findBible(bibleRequestDto)));
+
+            startPoint++;
+        }
+
+        if (bibleRangeReqDto.isAdd()) {
+            bibleStorage.addTodayBibleWords(bibleResponseDtoList);
+            return;
+        }
+
+        bibleStorage.updateTodayBibleWords(bibleResponseDtoList);
+    }
+
+    public void setBibleRangeByChapter(BibleRangeChapterReqDto bibleRangeChapterReqDto) {
+        int startPoint = bibleRangeChapterReqDto.getStartChapter();
+        int endPoint = bibleRangeChapterReqDto.getEndChapter();
+        try {
+            if (startPoint > endPoint) {
+                throw new NullPointerException();
+            }
+        } catch (NullPointerException e) {
+            log.info("NullPointerException = {}", e.getMessage());
+        }
+
+        BibleChapterRequestDto bibleChapterRequestDto = BibleChapterRequestDto.builder()
+                .book(bibleRangeChapterReqDto.getBook())
+                .build();
+
+        List<BibleResponseDto> bibleResponseDtoList = new ArrayList<>();
+        while (startPoint <= endPoint) {
+
+            bibleChapterRequestDto.setChapter(startPoint);
+            bibleResponseDtoList.addAll(bibleRepository.findBiblesByChapter(bibleChapterRequestDto).stream().map(BibleResponseDto::buildDto).toList());
+
+            startPoint++;
+        }
+
+        if (bibleRangeChapterReqDto.isAdd()) {
+            bibleStorage.addTodayBibleWords(bibleResponseDtoList);
+        }
+
         bibleStorage.updateTodayBibleWords(bibleResponseDtoList);
     }
 

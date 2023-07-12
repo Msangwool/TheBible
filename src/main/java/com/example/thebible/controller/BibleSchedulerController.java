30b3504173b55f6fd0ac7386345145feae67956a
OpenTheDoor <tkddnwkdb@gmail.com>
2023-07-13 00:43:04 +0900

    feat: API 추가, 절과 장 범위를 통해 오늘의 말씀 추가 가능

--- src/main/java/com/example/thebible/controller/BibleSchedulerController.java
+++ src/main/java/com/example/thebible/controller/BibleSchedulerController.java
@@ -1,14 +1,13 @@
 package com.example.thebible.controller;
 
-import com.example.thebible.dto.BibleRequestDto;
+import com.example.thebible.dto.BibleRangeChapterReqDto;
+import com.example.thebible.dto.BibleRangeReqDto;
 import com.example.thebible.service.BibleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,13 +17,26 @@ public class BibleSchedulerController {
 
     /**
      * 오늘의 성경 말씀을 등록합니다.
+     * <p>
+     * 오늘의 성경 말씀으로 등록할 절을 구간으로 작성합니다.
      *
-     * @param bibleRequestDtoList 오늘의 성경 말씀에 해당하는 모든 절을 받아옵니다.
-     * @return 성공하면 200 코드를 보냅니다.
+     * @param bibleRangeReqDto 오늘의 성경 말씀 범위를 알 수 있는 값을 받아옵니다.
+     * @return 성공했다면 200 코드를 보냅니다.
      */
     @PostMapping("/v1/bible/range")
-    public ResponseEntity<?> searchBibles(@RequestBody List<BibleRequestDto> bibleRequestDtoList) {
-        bibleService.setBiblesRequestRange(bibleRequestDtoList);
+    public ResponseEntity<?> uploadRangeBibles(@RequestBody BibleRangeReqDto bibleRangeReqDto) {
+        bibleService.setBibleRange(bibleRangeReqDto);
+        return ResponseEntity.ok().body(null);
+    }
+
+    /**
+     * 오늘의 성경 말씀을 등록합니다.
+     * <p>
+     * 오늘의 성경 말씀을 장 구간으로 작성합니다.
+     */
+    @PostMapping("/v1/bible/range/chapter")
+    public ResponseEntity<?> uploadRangeBiblesByChapter(@RequestBody BibleRangeChapterReqDto bibleRangeChapterReqDto) {
+        bibleService.setBibleRangeByChapter(bibleRangeChapterReqDto);
         return ResponseEntity.ok().body(null);
     }
 }

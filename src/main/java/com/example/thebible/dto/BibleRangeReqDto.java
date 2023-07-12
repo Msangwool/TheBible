18d253e7a357575a1051f60cb8766be103e394fc
OpenTheDoor <tkddnwkdb@gmail.com>
2023-07-12 23:52:58 +0900

    feat: Add BibleRangeReqDto

---
+++ src/main/java/com/example/thebible/dto/BibleRangeReqDto.java
@@ -0,0 +1,26 @@
+package com.example.thebible.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BibleRangeReqDto {
+    private int book;
+
+    private int chapter;
+
+    private int startVerse;
+    private int endVerse;
+    private boolean isAdd;
+
+    @Builder
+    public BibleRangeReqDto(int book, int chapter, int startVerse, int endVerse, boolean isAdd) {
+        this.book = book;
+        this.chapter = chapter;
+        this.startVerse = startVerse;
+        this.endVerse = endVerse;
+        this.isAdd = isAdd;
+    }
+}
